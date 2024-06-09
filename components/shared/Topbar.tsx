4ca4078e97ca1e72100104329e3f2a7afdfc59eb
Aastik Mandil <astik24mandilkota@gmail.com>
2024-06-09 10:59:52 +0530

    onboarding screen created (using mongoDB)

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 function Topbar() {
   const isUserLoggedIn = true;
@@ -32,6 +33,7 @@ function Topbar() {
 
         <OrganizationSwitcher
           appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: "py-2 px-4",
             },
